7f2cbdc47b3c04460c26d2ffff4fd8675144106f
Flora Li <floracitrus@gmail.com>
2017-08-22 18:47:53 +1000

    bubble sorted lab04 ex2 and lab05

--- lab04/ex2/routes.py
+++ lab04/ex2/routes.py
@@ -1,33 +1,45 @@
+# Import Flask Library
+#from flask import Flask
+# create a Flask application instance
+#app = Flask(__name__)
+# define a route through the app.route decorator
+#from server import app
+#@app.route("/")
+#def index():
+    #return "<h1>Hello World!</h1>"
+# launch the integrated development web server
+# and run the app on http://localhost:8085
+#if __name__=="__main__":
+#  app.run(debug=True,port=8085)
+#from server import app
+#@app.route("/")
+#def index():
+#    return "<h1> Hello world </h1>"
 from flask import Flask, redirect, render_template, request, url_for
 from server import app
 import csv
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form["name"]
-        zID = int(request.form["zID"])
-        desc = request.form["desc"]
+        integers = request.form["integers"].split(',')
+        final = bubbleSort(integers)
+        return render_template("second.html",final_list=final)
+    return render_template("first.html")
 
-        #user_input.append([name, zID, description])
-        with open('example.csv','a') as csv_out:
-        	writer = csv.writer(csv_out)
-        	writer.writerow([name, zID, desc])
-               
 
-        return redirect(url_for("hello"))
-    return render_template("index.html")
+def bubbleSort(li):
+    final_list = []
+    sorted = False  # We haven't started sorting yet
 
-@app.route("/Hello")
-def hello():
-	name = ""
-	id = 0
-	desc = ""
-	array = []
-	with open('example.csv','r') as csv_in:
-		reader = csv.reader(csv_in)
-		for row in reader:
-			name = row[0];
-			id = row[1];
-			desc = row[2];
-	return render_template("hello.html", name=name, id=id, desc=desc)
+    while not sorted:
+        sorted = True  # Assume the list is now sorted
+        for element in range(0, len(li)-1):
+            if int(li[element]) > int(li[element + 1]):
+                sorted = False  # We found two elements in the wrong order
+                temp = li[element + 1]
+                li[element + 1] = li[element]
+                li[element] = temp
+                final_list.append(list(li))
+    print(final_list)
+    return final_list 
 
